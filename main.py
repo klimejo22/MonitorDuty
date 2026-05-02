3ae41a548820f50e5efa117012340bb040e86216
klimejo22 <klimejo22@sps-prosek.cz>
2026-05-02 19:24:12 +0200

    Funkce navic

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import keyboard as kb
 
 def viewMonitor():
     kb.press_and_release('v+i+e+w+space+m+o+n+i+t+o+r')
+def switch():
+    kb.press_and_release('s+w+i+t+c+h')
+def transmit():
+    kb.press_and_release('t+r+a+n+s+m+i+t')
 
-kb.add_hotkey('f', viewMonitor, suppress=True)
+kb.add_hotkey('r', viewMonitor, suppress=True)
+kb.add_hotkey('f', switch, suppress=True)
+kb.add_hotkey('q', transmit, suppress=True)
+
 kb.wait()
